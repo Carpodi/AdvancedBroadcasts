af52668a78ebf632f769d838632f586e20e1cdbf
Carpodi <itsjahzielgarcia07@gmail.com>
2022-09-27 20:31:45 -0400

    Added console msg

--- src/main/java/xyz/carpodi/advancedbroadcasts/AdvancedBroadcasts.java
+++ src/main/java/xyz/carpodi/advancedbroadcasts/AdvancedBroadcasts.java
@@ -1,19 +1,35 @@
 package xyz.carpodi.advancedbroadcasts;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.carpodi.advancedbroadcasts.commands.BroadCastCommand;
 import xyz.carpodi.advancedbroadcasts.filemanagers.FileManager;
+import xyz.carpodi.advancedbroadcasts.utils.MessageColors;
 
 
 public final class AdvancedBroadcasts extends JavaPlugin {
 
     private FileManager config;
+    PluginDescriptionFile pdffile = getDescription();
+    public String version = this.pdffile.getVersion();
 
     @Override
     public void onEnable() {
         getCommand("broadcast").setExecutor((CommandExecutor) new BroadCastCommand(this));
 
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&7&m------------------------------------"));
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&aAdvancedBroadcasts &b"+version));
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&r "));
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&a Thanks for using AdvancedBroadcasts! :D"));
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&2 Created By: &fCarpodi&7, &fHappyRogelio7&7, &fKamila21Y&7, &fKaoryStudios&7, &fKaoryStudiosMC&7."));
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&r "));
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&a Server Version:&f "+Bukkit.getVersion()));
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&r "));
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&9&l Discord: &fhttps://discord.gg/3EebYUyeUX"));
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&d&l GitHub: &fhttps://github.com/KaoryStudios"));
+        Bukkit.getConsoleSender().sendMessage(MessageColors.getMsgColor("&7&m------------------------------------"));
         /*
         *Start code add by HappyRogelio7
         */
